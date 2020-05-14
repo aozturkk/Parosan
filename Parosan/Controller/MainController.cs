f7b753f4ddbbd97d396b24bdf4b066dda487e7e5
Ali Ozturk <aozturk@dijibt.com>
2020-05-15 00:36:34 +0300

    Add: Add Password User Controller .

---
+++ Parosan/Controller/MainController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Parosan.Controller
+{
+    class MainController
+    {
+
+        public static void callUserControl(Grid grid, UserControl userControl)
+        {
+            if(grid.Children.Count > 0)
+            {
+                grid.Children.Clear();
+                grid.Children.Add(userControl);
+            }
+            else
+            {
+                grid.Children.Add(userControl);
+            }
+        }
+    }
+}
--- Parosan/MainWindow.xaml.cs
+++ Parosan/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Parosan.Controller;
+using Parosan.View;
 
 namespace Parosan
 {
@@ -33,6 +35,16 @@ namespace Parosan
         private void minimize(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
+        }
+
+        private void passwordsTabClick(object sender, RoutedEventArgs e)
+        {
+            MainController.callUserControl(mainContent,new Password());
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainController.callUserControl(mainContent, new Password());
         }
     }
 }
