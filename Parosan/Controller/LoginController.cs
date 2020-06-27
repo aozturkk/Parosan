6d4a574d74b4ae5006aab4c0c1769d09543d3d30
Ali Ozturk <aozturk@dijibt.com>
2020-06-28 02:40:45 +0300

    ADD: add login page.

---
+++ Parosan/Controller/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parosan.Controller
+{
+    public class LoginController
+    {
+
+        private string databasePath = @"Data Source=" + Environment.CurrentDirectory + "\\db\\parosan.db;Version=3;New=false;Compress=True;Read Only=False";
+        SQLiteConnection dbConnection;
+
+        public bool checkUser(string username,string password)
+        {
+
+            dbConnection = new SQLiteConnection(databasePath);
+            dbConnection.Open();
+            SQLiteCommand sqlCommand = new SQLiteCommand("select * from user where username=@username and password = @password", dbConnection);
+
+
+            sqlCommand.Parameters.AddWithValue("@username", username);
+            sqlCommand.Parameters.AddWithValue("@password", password);
+            sqlCommand.ExecuteNonQuery();
+            SQLiteDataReader reader = sqlCommand.ExecuteReader();
+            int count = 0;
+
+            while (reader.Read())
+            {
+                count++;
+            }
+
+            if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+           
+        }
+    }
+}
---
+++ Parosan/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Parosan.Controller;
+
+namespace Parosan
+{
+    /// <summary>
+    /// LoginPage.xaml etkileşim mantığı
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void minimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            LoginController loginController = new LoginController();
+            
+            bool result = loginController.checkUser(usernameTextBox.Text,passwordTextBox.Text);
+
+            if (result) 
+            {
+                Console.WriteLine("login succesful !");
+            }
+            else
+            {
+                loginFaildLabel.Content = "Incorrect username or password !";
+            }
+        }
+
+       
+    }
+}
