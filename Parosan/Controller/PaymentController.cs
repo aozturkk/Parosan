260ae7fcfb1a39274532ae131cfc0e4372f2a290
Ali Ozturk <aozturk@dijibt.com>
2020-07-03 22:31:15 +0300

    ADD : Add Payment Model and Payment Controller.

---
+++ Parosan/Controller/PaymentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Parosan.Model;
+using Parosan.Service;
+
+namespace Parosan.Controller
+{
+    class PaymentController
+    {
+        private string databasePath = @"Data Source=" + Environment.CurrentDirectory + "\\db\\parosan.db;Version=3;New=false;Compress=True;Read Only=False";
+        SQLiteConnection dbConnection;
+        private static int lastID = 0;
+
+        public void connectionDB()
+        {
+
+            dbConnection = new SQLiteConnection(databasePath);
+            dbConnection.Open();
+
+
+        }
+
+        public List<PaymentModel> printPayment()
+        {
+
+            CryptoService cryptoService = new CryptoService();
+
+            connectionDB();
+            SQLiteCommand sqlCommand = new SQLiteCommand("select * from payment where user_id=" + UserModel.id, dbConnection);
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(sqlCommand);
+
+
+
+
+            List<PaymentModel> payment = new List<PaymentModel>();
+
+            
+
+            dbConnection.Close();
+            return payment;
+
+        }
+    }
+}
---
+++ Parosan/Model/PaymentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parosan.Model
+{
+    class PaymentModel
+    {
+        public int id { get; set; }
+        public String title { get; set; }
+        public String card_number { get; set; }
+        public String expiry_date { get; set; }
+        public String cvc { get; set; }
+        public String pin { get; set; }
+        public int user_id { get; set; }
+    }
+}
